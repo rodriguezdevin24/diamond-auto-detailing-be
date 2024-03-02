9845bb0990ab898926894efa2dbd632311a28aa3
rodriguezdevin24 <131619471+rodriguezdevin24@users.noreply.github.com>
2024-03-02 10:37:25 -0500

    completed the authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,5 +2,27 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
-require('../middleware/verifyToken');
+require('dotenv').config();
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({email});
+        if (!user) {
+            return res.status(404).json({ message: 'User not found'});
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(401).json({ message: 'Invalid credentials'});
+        }
+        const token = jwt.sign(
+            { userId: user._id },
+            process.env.JWT_SECRET,
+            { exipiresIn: '2h'} //token expires in 2 hours
+        );
+        res.json({ token });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
 
