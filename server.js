bfc3017a7db73d1cc944607c7929806363077a24
rodriguezdevin24 <131619471+rodriguezdevin24@users.noreply.github.com>
2024-03-04 12:03:22 -0500

    added routes and logic in server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const authRoute = require('./routes/authRoute')
 const userRoute = require('./routes/userRoute')
+const packageRoute = require('./routes/packageRoute')
+
 const testRoute = require('./routes/testRoute')
 const app = express();
 
@@ -11,8 +13,9 @@ require ('./db/connection')
 const PORT = process.env.port || 3200;
 
 app.use(express.json()); //Middleware for parsing JSON bodies
-app.use('/users', userRoute);
-app.use('/api/auth', authRoute)
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/packages', packageRoute);
 app.use('/test', testRoute);
 
 app.get('/', (req, res) => {
