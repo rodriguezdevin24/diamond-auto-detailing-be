d7fc887b26a9e834bbcc684b10aaaecb4639dcff
rodriguezdevin24 <131619471+rodriguezdevin24@users.noreply.github.com>
2024-03-04 12:03:22 -0500

    added routes and logic in server

--- routes/packageRoute.js
+++ routes/packageRoute.js
@@ -1,2 +1,21 @@
 const express = require('express')
 const router = express.Router();
+const packageController = require('../controllers/packageController');
+
+//Route for creating new package
+router.post('/', packageController.createPackage);
+
+//Route for getting all packages
+router.get('/', packageController.getAllPackages);
+
+//Route for getting package by ID
+router.get('/:id', packageController.getPackage);
+
+//Route for updating a package by ID
+router.put('/:id', packageController.updatePackage);
+
+//Route for deleting package by ID
+router.delete('/:id', packageController.deletePackage)
+
+module.exports= router;
+
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,4 +1,4 @@
-// userRoute.js
+// userRoute.js 
 
 const express = require('express');
 const router = express.Router();
